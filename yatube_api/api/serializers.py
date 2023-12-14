f0f24d80616ec7dc1c896e3b498b600483d03d19
Osipova228 <99545195+Osipova228@users.noreply.github.com>
2023-12-15 00:04:08 +0300

    PEP8

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -55,5 +55,7 @@ class FollowSerializer(serializers.ModelSerializer):
 
     def validate_following(self, value):
         if value == self.context['request'].user:
-            raise serializers.ValidationError('Нельзя подписаться на самого себя.')
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя.'
+            )
         return value
